27f924a5ab4cc72970094b8d8bd75d2d5b7baf99
SenhorContritus <eduardo.martinez@unesp.br>
2025-04-06 14:14:28 -0300

    parte de fora ta pog

--- components/Tarefa.tsx
+++ components/Tarefa.tsx
@@ -1,26 +1,69 @@
-import {Button,Text, View, StyleSheet} from "react-native"
+import {Button,Text, View, StyleSheet, Pressable} from "react-native"
 import _tarefa from "../types/_tarefas";
 import React from "react";
 
+
+
 type TarefaProp = {
     dados: _tarefa,
-    handleDeletePress: any
+    handleDeletePress: any,
 }
-
 export default function Tarefa(props:TarefaProp){
-    return  <View>
-                <Text>
+    let conc = false;
+
+    return  <View style={styles.div}>
+                <Text style={styles.conteudo}>
                     {props.dados.texto}
                 </Text>
-                <Button 
-                    color={'red'} 
-                    title="Excluir"
+                <Pressable 
+                    onPress={() => {
+                        conc = !(conc)
+                    }}
+                    style={({pressed}) => [{
+                            backgroundColor: pressed? "darkgreen" : 'green',
+                        },
+                        styles.button
+                    ]}
+                >
+                    <Text style={styles.textButton}>
+                        ✓
+                    </Text>
+                </Pressable>
+                <Pressable 
                     onPress={() => {props.handleDeletePress(props.dados.id)}}
-                />
+                    style={({pressed}) => [{
+                            backgroundColor: pressed? "darkred" : 'red',
+                        },
+                        styles.button
+                    ]}
+                >
+                    <Text style={styles.textButton}>
+                        X
+                    </Text>
+                </Pressable>
             </View>
 }
 const styles = StyleSheet.create({
     div:{
+        display:'flex',
+        flexDirection: 'row',
         borderWidth:1,
+        height: 50,
+
+
+    },
+    button:{
+        width: 30,
+        height: 30,
+        justifyContent: "center",
+        borderRadius: 6,
+    },
+    textButton:{
+        color: 'white',
+        textAlign: 'center',
+        justifyContent: "center",
+    },
+    conteudo:{
+
     }
 })
